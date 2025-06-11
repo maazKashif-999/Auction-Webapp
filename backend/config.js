888a89ed11ae21a1682c00415c4d505e19fd4290
maazKashif-999 <157843988+maazKashif-999@users.noreply.github.com>
2025-06-11 13:14:26 +0500

    Delete config.js

--- backend/config.js
+++
@@ -1,2 +0,0 @@
-export const MongoDbConnection = "mongodb+srv://root:cs300@auctionapp.j7ru4hq.mongodb.net/auction?retryWrites=true&w=majority&appName=AuctionApp"
-export const JWT_SECRET_KEY = "CS300AUCTION";
